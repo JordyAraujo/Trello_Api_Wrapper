ff86a5eed19f1c14c52194ecea663118b6b79e2e
jordyaraujo <jordyaraujo@outlook.com>
2022-07-12 16:53:39 -0300

    Refactor: Run pre-commit - Create public functions for both Trello and Board Classes

--- trelloapi/__init__.py
+++ trelloapi/__init__.py
@@ -1,43 +1,48 @@
+"""Library initializer"""
 import httpx
 
+
 class Trello:
+    """Trello class definition. It holds and interacts with the Boards."""
+
     def __init__(self, apikey, token):
+        """Class constructor. Boards are set when the request is successful."""
         self._apikey = apikey
         self._token = token
+        self.set_boards()
 
+    def list_boards(self):
+        """Requests all Boards the current user has from Trello API."""
+        try:
+            url = "https://api.trello.com/1/members/me/boards"
+
+            headers = {"Accept": "application/json"}
+
+            params = {"key": self._apikey, "token": self._token}
+
+            response = httpx.get(url, headers=headers, params=params)
+            response.raise_for_status()
+            return {
+                "status": response.status_code,
+                "boards": [
+                    {
+                        "id": board["id"],
+                        "name": board["name"],
+                    }
+                    for board in response.json()
+                ],
+            }
+        except httpx.HTTPStatusError as exc:
+            return {
+                "status": exc.response.status_code,
+                "url": f"{exc.request.url}",
+            }
+
+    def set_boards(self):
+        """Set an updated list of Boards for this User."""
         boards = self.list_boards()
 
         if boards["status"] == 200:
             self.board_list = boards["boards"]
         else:
             self.board_list = None
-
-
-    def list_boards(self):
-        try:
-            url = "https://api.trello.com/1/members/me/boards"
-
-            headers = {
-                "Accept": "application/json"
-            }
-
-            params = {
-                'key': self._apikey,
-                'token': self._token
-            }
-
-            response = httpx.get(url, headers=headers, params=params)
-            response.raise_for_status()
-            return({
-                "status": response.status_code,
-                "boards": [{
-                    "id": board["id"],
-                    "name": board["name"],
-                    } for board in response.json()
-                ]
-            })
-        except httpx.HTTPStatusError as exc:
-            return({
-                "status": exc.response.status_code,
-                "url": f"{exc.request.url}"
-            })
--- trelloapi/board.py
+++ trelloapi/board.py
@@ -1,3 +1,15 @@
-class Board():
+"""Board class definition. It holds and interacts with the Cards."""
+
+
+class Board:
+    """Board class definition. It holds and interacts with the Cards."""
+
     def __init__(self, board_id):
+        """Class constructor."""
         self.board_id = board_id
+
+    def list_cards(self):
+        """Requests all Cards this Board has from Trello API."""
+
+    def set_cards(self):
+        """Set an updated list of Cards for this Board."""
